e486862c3cccce23f3babad90bbeadfe4f57afac
W3endigo <smartcoin2102@gmail.com>
2024-02-22 15:38:43 +0100

    :loud_sound: refacto logs

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
+import MySQLdb
 from flask import Flask, request, redirect
 from flask.templating import render_template
 from flask_mysqldb import MySQL
+from colorama import init as colorama_init
+from colorama import Fore
+from colorama import Style
+import logging
+from datetime import datetime
+
+colorama_init()
 
 app = Flask(__name__)
 app.debug = True
 app.config['DEBUG'] = True
 
+# Logging configuration
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 # MySQL configurations
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_PORT'] = 3307
@@ -16,17 +27,23 @@ app.config['MYSQL_DB'] = 'slot_machine'
 # Creating a MySQL instance
 mysql = MySQL(app)
 
+print(f"{Fore.YELLOW}{Style.BRIGHT}Lancement de l'application : {datetime.now().strftime('%H:%M:%S')}{Fore.RESET}")
+
 
 @app.route('/')
 def index():
     #   Recuperation des données de la table joueurs (sans prendre en compte le mot de passe)
     print("Recuperation des données de la table joueurs (sans prendre en compte le mot de passe)")
-    cursor = mysql.connection.cursor()
-    cursor.execute("SELECT id_joueur, nom, prenom, nb_credits FROM joueurs")
-    joueurs = cursor.fetchall()
-    cursor.close()
-    print(joueurs)
-    return render_template('index.html', joueurs=joueurs)
+    try:
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT id_joueur, nom, prenom, nb_credits FROM joueurs")
+        joueurs = cursor.fetchall()
+        cursor.close()
+        return render_template('index.html', joueurs=joueurs)
+    except MySQLdb.OperationalError:
+        print(f"{Fore.RED}Erreur de connexion à la base de donées{Fore.RESET}")
+
+        return render_template('index.html', joueurs=[])
 
 
 @app.route('/register')
